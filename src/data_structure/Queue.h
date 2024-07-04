98ee9ed2c5c1e337fea3485a19867419e5d76f8b
Your Name <alittlefu@163.com>
2024-07-04 16:44:07 +0800

    update stack

--- src/data_structure/Queue.h
+++ src/data_structure/Queue.h
@@ -4,30 +4,77 @@
 #include"BinTree.h"
 
 #define Error nullptr
-template<typename QElementType>
+template<typename QValueType>
 struct QNodeVal{
-    QElementType Data;
+    QValueType Data;
     struct QNodeVal *next{nullptr};
 };
-template<typename QElementType>
+template<typename QValueType>
 struct QNode{
-    struct QNodeVal<QElementType> *rear{nullptr};
-    struct QNodeVal<QElementType> *front{nullptr};
+    struct QNodeVal<QValueType> *rear{nullptr};
+    struct QNodeVal<QValueType> *front{nullptr};
 };
 
-template<typename QElementType>
+template<typename QValueType>
 class Queue{
 private:
-    QNode<QElementType>* PtrQ;
+    QNode<QValueType>* PtrQ;
 public:
-    Queue():PtrQ{new QNode<QElementType>} {};
+    Queue():PtrQ{new QNode<QValueType>} {};
     ~Queue(){};
     //创建新队列
     void CreateQueue();
     //判断队列是否为空
     bool IsEmpty();
     //向队列中添加元素
-    void AddQ(QElementType X);
+    void AddQ(QValueType X);
     //从队列中删除元素
-    QElementType DeleteQ();
+    QValueType DeleteQ();
 };
+
+//创建新队列
+template<typename QValueType>
+void Queue<QValueType>::CreateQueue(){
+    PtrQ->front = PtrQ->rear = nullptr;
+}
+//判断队列是否为空
+template<typename QValueType>
+bool Queue<QValueType>::IsEmpty(){
+    return (PtrQ->front == nullptr);
+}
+//向队列中添加元素
+template<typename QValueType>
+void Queue<QValueType>::AddQ(QValueType X){
+    QNodeVal<QValueType> *FrontCurr = new QNodeVal<QValueType>;
+    FrontCurr->Data = X;
+    if(IsEmpty()){
+        FrontCurr->next = PtrQ->front;
+        PtrQ->front = FrontCurr;
+        PtrQ->rear = FrontCurr;
+    }else{
+        PtrQ->rear->next = FrontCurr;
+        PtrQ->rear = FrontCurr;
+    }
+}
+//从队列中删除元素
+template<typename QValueType>
+QValueType Queue<QValueType>::DeleteQ(){
+    QNodeVal<QValueType> *FrontCell = new QNodeVal<QValueType>;
+    QValueType FrontElem;
+    // if(IsEmpty()){
+    //     std::cout<<"队列空！";
+    //     return Error;
+    // }
+    // else{
+        FrontCell = PtrQ->front;
+        if(PtrQ->front == PtrQ->rear){
+            PtrQ->front = PtrQ->rear = nullptr;
+        }else{
+            PtrQ->front = PtrQ->front->next;
+        }
+        FrontElem = FrontCell->Data;
+        delete FrontCell;
+        FrontCell = nullptr;
+        return FrontElem;
+    // }
+}
--- src/data_structure/Stack.h
+++ src/data_structure/Stack.h
@@ -1,6 +1,4 @@
 #pragma once
-typedef int ElementType;
-typedef int Position;
 
 template <typename SValueType>
 struct SNode{
@@ -11,9 +9,9 @@ struct SNode{
 template <typename SValueType>
 class Stack{
 private:
-    SNode<SValueType>* S;
+    SNode<SValueType>* SNPtr;
 public:
-    Stack(){S = nullptr;};
+    Stack(){SNPtr = nullptr;};
     ~Stack(){};
     //判断栈是否空
     bool IsEmpty ();
@@ -26,28 +24,28 @@ public:
 //判断栈是否空
 template <typename SValueType>
 bool Stack<SValueType>::IsEmpty (){
-    return (S == nullptr);
+    return (SNPtr == nullptr);
 }
 
 //向栈中压入元素
 template <typename SValueType>
 void Stack<SValueType>::Push( SValueType item ){
-    SNode<SValueType>* S_tmp = new SNode<SValueType>();
-    S_tmp->value = item;
-    S_tmp->next = S;
-    S = S_tmp;
+    SNode<SValueType>* SNPtr_tmp = new SNode<SValueType>();
+    SNPtr_tmp->value = item;
+    SNPtr_tmp->next = SNPtr;
+    SNPtr = SNPtr_tmp;
 }
 //从栈中弹出元素
 template <typename SValueType>
 SValueType Stack<SValueType>::Pop(){
     try{
-        if(S != nullptr){
+        if(SNPtr != nullptr){
             SValueType pop_value;
-            SNode<SValueType>* S_tmp{S};
-            S = S->next;
-            pop_value = S_tmp->value;
-            delete S_tmp;
-            S_tmp = nullptr;
+            SNode<SValueType>* SNPtr_tmp{SNPtr};
+            SNPtr = SNPtr->next;
+            pop_value = SNPtr_tmp->value;
+            delete SNPtr_tmp;
+            SNPtr_tmp = nullptr;
             return pop_value;
         }else{
             throw "堆栈为空！";
